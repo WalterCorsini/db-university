74a0e5ce65ae38a18ea95985c06b3505dc0aee68
WalterCorsini <corsini.walter@gmail.com>
2024-05-30 15:35:53 +0200

    add n.4  group by

--- query.sql
+++ query.sql
@@ -95,3 +95,9 @@ HAVING COUNT(*) > 1;
 SELECT `exam_id`, AVG(`vote`)
 FROM `exam_student`
 GROUP BY `exam_id`;
+
+-- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
+
+SELECT `degrees`.`department_id`, COUNT(*)
+FROM `degrees`
+GROUP BY `degrees`.`department_id`;
